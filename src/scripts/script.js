2d658c083ae7b9d176fdb228d043cc6aa081002f
fadesignlk <fadesg001@gmail.com>
2024-07-07 01:28:04 +0530

    added form registration

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -20,3 +20,15 @@ function topFunction() {
 
  // JavaScript to get the current year and insert it into the HTML
  document.getElementById('year').textContent = new Date().getFullYear();
+
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbzzPW6HXwCmtwd86KH0iBEvpKy_wZ4Aeb3eLw4IaN9B_Z9_b0fHFZhwb2d3brQMli04/exec'
+
+ const form = document.forms['volunteer-register']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you for registering with us. We'll get in touch with you soon" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
